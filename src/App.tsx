c765d508cdfd949815a604af08838976ae0c600a
zoe <jsh010322@gmail.com>
2025-02-17 23:04:43 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import theme from "./theme";
+
+const HeaderTextDiv = styled.div`
+  background-color: ${theme.primary};
+  width: 320px;
+  color: white;
+  padding: 16px 0;
+  text-align: center;
+  border-radius: 8px;
+`;
+
+const HeaderDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 32px 64px 0;
+`;
+
+const ChartTitleDiv = styled.div`
+  font-size: large;
+`;
+
+export default function Home() {
+  return (
+    <div>
+      <HeaderDiv>
+        <HeaderTextDiv>ID:asldkasjdlaskd</HeaderTextDiv>
+
+        <HeaderTextDiv>test hospital</HeaderTextDiv>
+      </HeaderDiv>
+    </div>
+  );
+}
---
+++ src/theme.ts
@@ -0,0 +1,5 @@
+const theme = {
+    primary: "#426B1F"
+}
+
+export default theme
